573df8faec3fa3697ec7fd09f2d5f890209a6562
omer-edelstain <omer.edelstain@gmail.com>
2015-08-15 20:42:03 +0300

    first commit family member activity

--- FamilyTracker/app/src/main/java/il/ac/huji/familytracker/FTFamilyMemberActivity.java
+++ FamilyTracker/app/src/main/java/il/ac/huji/familytracker/FTFamilyMemberActivity.java
@@ -1,11 +1,16 @@
 package il.ac.huji.familytracker;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class FTFamilyMemberActivity extends ActionBarActivity {
@@ -17,19 +22,120 @@ public class FTFamilyMemberActivity extends ActionBarActivity {
     private EditText _edtName;
     private EditText _edtEMail;
     private EditText _edtNumber;
+    private TextView _txtName;
+    private TextView _txtEMail;
+    private TextView _txtNumber;
+    private TextView _txtHeader;
 
-    private Button btnImport;
+    private LinearLayout _lnrlytNameInputSection;
+    private Button btnExtendedEditingOpts;
+    private Button btnChangeFrontActivity;
 
     private FamilyMember _currentMember;
-
+    private Boolean m_blnIsEditMode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ftfamily_member);
 
+        //saving references to visual elements necessery for the setting of the activity's mode
+        //edit boxes
+        _edtEMail = (EditText) findViewById(R.id.email_address);
+        _edtName = (EditText) findViewById(R.id.name);
+        _edtNumber = (EditText) findViewById(R.id.phone_number);
+        //edit boxes' labels
+        _txtEMail = (TextView) findViewById(R.id.Email_Label_Id);
+        _txtName = (TextView) findViewById(R.id.Name_Label_Id);
+        _txtNumber = (TextView) findViewById(R.id.Phone_Label_Id);
+        _txtHeader = (TextView) findViewById(R.id.Member_Activity_Header);
+        _lnrlytNameInputSection = (LinearLayout) findViewById(R.id.Name_Edit_Section);
+        Intent intntCurrActivity = getIntent();
+        m_blnIsEditMode = intntCurrActivity.getBooleanExtra("IsFirstCreated", true);
+        SetActivityState();
+
     }
 
+    private void SetActivityState() {
+        //TODO if activity was opened on edit mode
+        //button with id continue from activity has dual meaning - when activity in edit mode -
+        //it means confirm changes
+        //if in view mode , opens map view activity with currrent member's coordinates
+        // according to the activity state -set editability for each editText
+        SetEditTextEnabledState(_edtNumber);
+        SetEditTextEnabledState(_edtName);
+        SetEditTextEnabledState(_edtNumber);
+        _txtHeader.setText(setHeaderLabelContent());
+        _lnrlytNameInputSection.setVisibility(m_blnIsEditMode ? LinearLayout.VISIBLE : LinearLayout.GONE);
+        setButtonsPerActivityState();
 
+    }
+
+    private void setButtonsPerActivityState() {
+        View.OnClickListener lstnEditOpsHandler;
+        View.OnClickListener lstnFrontActivityChangeHandler;
+        SetImageToButton();
+        if (m_blnIsEditMode) {
+            lstnEditOpsHandler = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    OpenDataImportDialog(v);
+                }
+            };
+            lstnFrontActivityChangeHandler = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ConfirmDataInputHandler(v);
+                }
+            };
+        } else {
+            lstnEditOpsHandler = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    OpenMemberEditWindow(v);
+                }
+            };
+            lstnFrontActivityChangeHandler = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    OpenMapHandler(v);
+                }
+            };
+        }
+
+        btnExtendedEditingOpts.setOnClickListener(lstnEditOpsHandler);
+        btnChangeFrontActivity.setOnClickListener(lstnFrontActivityChangeHandler);
+    }
+
+    private void SetImageToButton() {
+        //TODO check link http://stackoverflow.com/questions/17100583/where-are-the-drawables-or-clipart-from-android-r-drawable-stored
+        // and learn best way to integrate icons into app
+    }
+
+    private String setHeaderLabelContent() {
+        return m_blnIsEditMode ? (String) getResources().getText(R.string.Edit_Mode_Header) : _currentMember.getName();
+    }
+
+    private void OpenMapHandler(View p_vwCurrentCallbackData) {
+        //TODO use intent of map and coordinates from service , or from the user data loaded to activity
+    }
+
+    private void ConfirmDataInputHandler(View p_vwCurrentCallbackData) {
+    }
+
+    private void SetEditTextEnabledState(EditText p_edtToDisable) {
+        p_edtToDisable.setEnabled(m_blnIsEditMode);
+        p_edtToDisable.setFocusable(m_blnIsEditMode);
+        p_edtToDisable.setClickable(m_blnIsEditMode);
+    }
+
+    void OpenDataImportDialog(View p_vwCurrentCallbackData) {
+        //TODO decide on the logic in this activity ,and it's nature-a modal dialog ,or a seperate
+        // activity
+    }
+
+    void OpenMemberEditWindow(View p_vwCurrentCallbackData) {
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- FamilyTracker/app/src/main/java/il/ac/huji/familytracker/FamilyMember.java
+++ FamilyTracker/app/src/main/java/il/ac/huji/familytracker/FamilyMember.java
@@ -1,5 +1,7 @@
 package il.ac.huji.familytracker;
 
+import java.util.Date;
+
 /**
  * Created by Tanyagizell on 23/07/2015.
  *
@@ -9,13 +11,43 @@ package il.ac.huji.familytracker;
 public class FamilyMember {
 
 
-    String _name;
+    private String m_strName;
+    private String m_strPhoneNumber;
+    private String m_strEmail;
+    private String m_strLatestCoord;
     //TODO list of locations
 
 
-    public FamilyMember(String name)
+    public FamilyMember(String p_strName, String p_strPhone, String p_strEmail)
     {
-        _name = name;
+        m_strName = p_strName;
+        m_strPhoneNumber = p_strPhone;
+        m_strEmail = p_strEmail;
     }
 
+    public String getName() {
+        return m_strName;
+    }
+
+    public String getPhoneNumber() {
+        return m_strPhoneNumber;
+    }
+
+    public String getEmail() {
+        return m_strEmail;
+    }
+
+    public void setEmail(String p_strEmail) {
+        this.m_strEmail = p_strEmail;
+    }
+
+    public String getLatestCoord() {
+        //TODO add time stamp -possibility
+        // or decide on a modus operndi r
+        return m_strLatestCoord;
+    }
+
+    public void setLatestCoord(String m_strLatestCoord) {
+        this.m_strLatestCoord = m_strLatestCoord;
+    }
 }
